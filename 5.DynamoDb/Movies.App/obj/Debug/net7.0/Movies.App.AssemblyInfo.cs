444e21f60a2cf2d54f5cea6708113b7074eec41a
ShindQQ <denis110402@gmail.com>
2024-09-12 10:55:44 +0300

    got to know AWS S3

--- 5.DynamoDb/Movies.App/obj/Debug/net7.0/Movies.App.AssemblyInfo.cs
+++ 5.DynamoDb/Movies.App/obj/Debug/net7.0/Movies.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66a0f456444b308b80c7d5c7f9cf68cb005e2c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
